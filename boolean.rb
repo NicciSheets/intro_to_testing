799ca3e47a65c5da0e14450104761c3ef956d958
Nicci Sheets <niccisheets@aol.com>
2018-06-20 11:03:18 -0400

    wrote def for comparing classes

--- boolean.rb
+++ boolean.rb
@@ -10,3 +10,11 @@ comparison(2,2)
 comparison(1,2)
 comparison(22,22)
 comparison(21,22)
+
+
+
+
+ 
+comparison("Ruby".class,1.class)
+comparison("1.00".class,true.class)
+comparison("Ruby".class,"Ruby".class)
