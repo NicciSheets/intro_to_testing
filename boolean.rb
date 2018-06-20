b73967790f1a1984d4aafd994dad09993d8d8885
Nicci Sheets <niccisheets@aol.com>
2018-06-20 11:46:23 -0400

    evaluates if same value, IF same class

--- boolean.rb
+++ boolean.rb
@@ -1,29 +1,53 @@
-def comparison(num1,num2)
-	if num1 == num2
+def comparison(a,b)
+	if a == b
 		puts "True"
 	else
 		puts "False"
 	end
 end
 
-comparison(2,2)
-comparison(1,2)
-comparison(22,22)
-comparison(21,22)
+#comparison(2,2)
+#comparison(1,2)
+#comparison(22,22)
+#comparison(21,22)
 
 
 
 def class_comparison(a,b)
 	if a.class == b.class
-		puts "True"
-	else
-		puts "False"
+			if a == b
+				return "True"
+			else 
+				return "False"
+			end
+		else
+		return "False"
 	end
 end
 
- 
-class_comparison("Ruby",1)
-class_comparison("1.00",true)
-class_comparison("Ruby","Ruby")
-class_comparison("Ruby","Programming")
+p class_comparison("Ruby",1)
+p class_comparison("Ruby","Ruby") 
+p class_comparison("Ruby", "Programming")
+
+#class_comparison("Ruby",1)
+#class_comparison("1.00",true)
+#class_comparison("Ruby","Ruby")
+#class_comparison("Ruby","Programming")
+
+#class_comparison("Ruby",1) && comparison("Ruby",1)
+#class_comparison("Ruby","Ruby") && comparison("Ruby","Ruby")
+#class_comparison("Ruby","Programming") && comparison("Ruby","Programming")
+
+=begin
+def value_comparison(class_comparison(a,b),comparison(a,b))
+	"class_comparison(a,b)#{class_comparison(a,b)}"
+	"comparison(a,b)#{comparison(a,b)}"
+	if class_comparison == true
+		return "#{comparison}"
+	else class_comparison == false 
+		return "#{comparison}"
+	end
+end
+=end
+
 
